9f7e48548ee8b58e8ac1df7e646d3370f40c1284
shauni99 <36627000+shauni99@users.noreply.github.com>
2019-12-10 20:00:18 +0100

    ingredientDatabaseDAO + cocktaildatabase

--- app/src/main/java/com/example/cocktailapp/Model/Ingredient.kt
+++ app/src/main/java/com/example/cocktailapp/Model/Ingredient.kt
@@ -2,9 +2,12 @@ package com.example.cocktailapp.Model
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "ingredient_table")
 data class Ingredient(
+    @PrimaryKey(autoGenerate = false)
+    var idIngredient: String? = "",
     @ColumnInfo(name = "name")
     val name: String?,
     @ColumnInfo(name = "measure")
