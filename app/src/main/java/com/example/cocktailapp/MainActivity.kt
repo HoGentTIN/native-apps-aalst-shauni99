c02df4fbffe9be77b4c5347a2ee49a52ba2f17b3
shauni99 <36627000+shauni99@users.noreply.github.com>
2019-12-10 18:49:03 +0100

    bottom navigatiebalk

--- app/src/main/java/com/example/cocktailapp/MainActivity.kt
+++ app/src/main/java/com/example/cocktailapp/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.cocktailapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var navHostFragment: NavHostFragment
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.myNavHostFragment) as NavHostFragment
+
+        bottom_nav_view.setupWithNavController(navController = navHostFragment.navController)
     }
 }
